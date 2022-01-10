2ef359f1a67e19de6f347c8d771b7a62a916e4c4
Вячеслав <goplit2010.konovalov@yandex.ru>
2022-01-10 19:21:35 +0300

    add rocket

--- ping_pong.py
+++ ping_pong.py
@@ -33,5 +33,50 @@ for i in range(25):
         border.down()
 border.hideturtle()
 
+# создание ракеток
+rocket_left = turtle.Turtle()
+rocket_left.color('white')
+rocket_left.shape('square')
+rocket_left.shapesize(stretch_len=1, stretch_wid=5)
+rocket_left.penup()
+rocket_left.goto(-450, 0)
+
+rocket_right = turtle.Turtle()
+rocket_right.color('white')
+rocket_right.shape('square')
+rocket_right.shapesize(stretch_len=1, stretch_wid=5)
+rocket_right.penup()
+rocket_right.goto(450, 0)
+
+def move_up_left():
+    y = rocket_left.ycor() + 10
+    if y > 240:
+        y= 240
+    rocket_left.sety(y + 10)
+
+def move_down_left():
+    y = rocket_left.ycor() - 10
+    if y < -240:
+        y= -240
+    rocket_left.sety(y - 10)
+
+def move_up_right():
+    y = rocket_right.ycor() + 10
+    if y > 240:
+        y= 240
+    rocket_right.sety(y + 10)
+
+def move_down_right():
+    y = rocket_right.ycor() - 10
+    if y < -240:
+        y= -240
+    rocket_right.sety(y - 10)
+
+window.listen()
+window.onkeypress(move_up_left, 'w')
+window.onkeypress(move_down_left, 's')
+window.onkeypress(move_up_right, 'Up')
+window.onkeypress(move_down_right, 'Down')
+
 
 window.mainloop()
